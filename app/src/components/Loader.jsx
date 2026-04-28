5a8678e04ed3cdac6cbd69adb223990c34d5c997
HadssanK <hassankhan847754@gmail.com>
2026-04-28 23:46:39 +0500

    updated

--- app/src/components/Loader.jsx
+++ app/src/components/Loader.jsx
@@ -9,8 +9,10 @@ export const Loader = () => {
             <div className="absolute animate-spin rounded-full h-[6.5rem] w-[6.5rem] border-t-4 border-b-4 border-[#1E7773]"></div>
             <Image
                 src={`${Image_Url}CircleSliderAssets/circleLogo.svg`}
+                width={96}
+                height={96}
                 className="rounded-full h-24 w-24"
-                alt="Loading Logo" 
+                alt="Loading Logo"
             />
         </div>
     );
