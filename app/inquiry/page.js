362dc1472fff225476c78d7fb85051ae04f35673
HadssanK <hassankhan847754@gmail.com>
2026-04-29 16:07:02 +0500

    updated

--- app/inquiry/page.js
+++ app/inquiry/page.js
@@ -4,7 +4,7 @@
 import React, { Suspense } from "react";
 import Inquiry from "../src/Pages/Invoice"
 
-export const revalidate = 86400;
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
